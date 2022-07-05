504928aadd30c4df531d4843922422af4d8d9640
jmh0036 <jmh0036@tigermail.auburn.edu>
2022-07-05 10:14:56 -0400

    Orthogonality Tester

--- DoubleCheck.py
+++ DoubleCheck.py
@@ -1,3 +1,5 @@
+import copy
+
 def IsOrthogonal(grid1,grid2):
     SetOfOrderedPairs = set()
     for i in range(len(grid1)):
@@ -9,21 +11,68 @@ def IsOrthogonal(grid1,grid2):
         return True
 
 def AreMutuallyOrthogonal(arrayOfarrays):
-    # if arrayOfarrays == []:
-    #     return True
+    if arrayOfarrays == []:
+        return True
     for i in range(len(arrayOfarrays)-1):
-        for j in range(i+1,len(arrayOfarrays))
-        if IsOrthogonal(i,j) == False:
-            return False
+        for j in range(i+1,len(arrayOfarrays)):
+            if IsOrthogonal(arrayOfarrays[i],arrayOfarrays[j]) == False:
+                return False
     return True
 
+def Normalize(arr):
+    LocalDict = dict()
+    for i in range(len(arr)):
+        LocalDict[arr[0][i]] = i
+    LocalArray = copy.deepcopy(arr)
+    for i in range(len(LocalArray)):
+        for j in range(len(LocalArray)):
+            LocalArray[i][j] = LocalDict[LocalArray[i][j]] + 1 
+    return LocalArray
 
-MutOrthSet = [
-    [
-        [1, 2, 4, 3, 6, 7, 5, 4],
-        [4, 5, 7, 6, 2, 3, 1, 0]
-    ]
+array1 = [
+    [0,1,3,2,6,7,5,4],
+    [4,5,7,6,2,3,1,0],
+    [2,3,1,0,4,5,7,6],
+    [6,7,5,4,0,1,3,2],
+    [5,4,6,7,3,2,0,1],
+    [1,0,2,3,7,6,4,5],
+    [7,6,4,5,1,0,2,3],
+    [3,2,0,1,5,4,6,7]
 ]
 
+array2 = [
+    [0,1,2,3,5,4,7,6],
+    [4,5,6,7,1,0,3,2],
+    [6,7,4,5,3,2,1,0],
+    [2,3,0,1,7,6,5,4],
+    [3,2,1,0,6,7,4,5],
+    [7,6,5,4,2,3,0,1],
+    [5,4,7,6,0,1,2,3],
+    [1,0,3,2,4,5,6,7]
+]
 
-print(AreMutuallyOrthogonal(MutOrthSet))
+array3 = [
+    [0,1,2,3,4,5,6,7],
+    [4,5,6,7,0,1,2,3],
+    [2,3,0,1,6,7,4,5],
+    [6,7,4,5,2,3,0,1],
+    [1,0,3,2,5,4,7,6],
+    [5,4,7,6,1,0,3,2],
+    [3,2,1,0,7,6,5,4],
+    [7,6,5,4,3,2,1,0]
+]
+
+Originals = [array1,array2,array3]
+
+Normalized = [Normalize(array1),Normalize(array2),Normalize(array3)]
+
+print( AreMutuallyOrthogonal( Originals ) )
+
+print('')
+
+for i in Normalized:
+    for j in i:
+        print(j)
+    print('')
+
+print( AreMutuallyOrthogonal( Normalized ) )
